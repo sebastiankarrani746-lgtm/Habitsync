612710111de7709c50879fe0e1734ef65e4fe82d
sebbekarrani@hotmail.com <sebbekarrani@hotmail.com>
2026-05-12 10:13:12 +0200

    models

---
+++ backend/models.py
@@ -0,0 +1,23 @@
+from sqlalchemy import Integer, Column, String, Boolean, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+from backend.database import Base
+
+class User (Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+    habits = relationship("Habit", back_populates="owner")
+
+class Habit (Base): 
+    __tablename__ = "habits"
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    description = Column(String)
+    is_completed = Column(Boolean, default=False)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    
+    owner = relationship("User", back_populates="habits")
